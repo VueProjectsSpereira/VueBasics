ec257f21c9d6c3c8e115018fdbbffc59a52ab9ee
Saul Pereira <saul_pereira@hotmail.com>
2024-09-11 15:08:33 -0600

    22. Outputting RAW HTML Content with v-html
    22. Outputting RAW HTML Content with v-html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoalA: "Finish the course and learn Vue!",
-      courseGoalB: "Master Vue and build amazaing apps!",
+      courseGoalB: "Master Vue and build amazing apps!",
       vueLink: "https://vuejs.org/",
     };
   },
